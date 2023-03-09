ced995bb9c21002a81ffea2d78802387b2b30a2a
Paola <botinapaola35@gmail.com>
2023-03-08 20:03:31 -0500

    prueba

---
+++ main.py
@@ -0,0 +1,28 @@
+# tipos de datos
+# Numeros Enteros
+
+
+numero1: int = 78
+numero2 = 45
+
+print(numero1)
+print(numero2)
+
+# Numeros reales
+
+numero3: float = 4.9
+numero4 = 5.0
+
+print(numero3)
+print(numero4)
+
+# Booleanos toman dos valores verdadero o falso
+
+esColombiano: bool = True
+esArgentino = False
+
+print(esColombiano)
+print(esArgentino)
+
+# caracter y Cadena de caracteres
+mensaje = "cadena con una comilla simple ', una comilla doble \" y una diagonal invertida "
